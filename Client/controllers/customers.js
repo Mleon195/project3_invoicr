76115c3b69b0ff59ef4391e91726af24fc21ecfe
Mleon195 <mleon@mail.ccsf.edu>
2016-06-01 14:00:01 -0700

    customers/controllers and view

---
+++ Client/controllers/customers.js
@@ -0,0 +1,5 @@
+var myApp = angular.module("myApp");
+
+myApp.controller('CustomersController', ['$scope','$http','$location', function($scope, $http, $location){
+    console.log('Customers Controller Initialized...');
+}]);
